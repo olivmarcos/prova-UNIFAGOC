cd23fc1504cb1894dd5b8cbd023dea0e9d72d081
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2019-11-30 23:12:42 -0300

    tela de login

--- public/app/Views/Home.php
+++ public/app/Views/Home.php
@@ -1,48 +1,31 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Autocomplete - Default functionality</title>
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
-
-  <link rel="stylesheet" href="<?= ASSET ?>">
-
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="<?= ASSET ?>">
+   <title>Document</title>
 </head>
 <body>
+   <header></header>
+   <section>
 
-<label>Nome:</label>
-<select name="" id="selUser"></select>
-
-<script>
-$(document).ready(function(){
-
-$("#selUser").select2({
-   ajax: {
-     url: "/select",
-     type: "get",
-     dataType: 'json',
-     delay: 250,
-     data: function (params) {
-        return {
-           searchTerm: params.term // search term
-        };
-     },
-     processResults: function (response) {
-        return {
-           results: response
-        };
-     },
-     cache: true
-   }
-});
-});
-</script>
-
+      <form id="login" action="" method="post">
+         <div class="titulo">
+            <h2>Sistema de Cadastro</h2>
+         </div>
+         <div class="login-item">
+            <label for="">Nome</label><br>
+            <input type="text">
+         </div>
+         <div class="login-item">
+            <label for="">Senha</label><br>
+            <input type="text">
+         </div>
+         <button type="submit">Entrar</button>
+      </form>
+   </section>
+   <footer></footer>
 </body>
 </html>
